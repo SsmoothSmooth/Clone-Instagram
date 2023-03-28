cbf96029bccd57879d4b8422f95fabdfeb9e7a1e
José Wilson de Lima <josewilson791@gmail.com>
2023-03-27 22:54:13 -0300

    :sparkles: feat:
    Pagina inicial

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 function App() {
 
   // Verificar em qual status o usuário se encontra logado ou não
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState("Smooth");
 
   useEffect(()=>{
   
@@ -24,7 +24,10 @@ function App() {
 
           {
             (user)?
-            <div>Hi</div>
+            <div className="header_logadoInfo">
+              <span>Hi, <b>{user}</b></span>
+              <a href="#">Postar</a>
+            </div>
             :
 
             <div className="header_loginForm">
@@ -33,6 +36,10 @@ function App() {
                 <input type="password" placeholder="Senha.."/>
                 <input type="submit" name="acao" value="Logar!"/>
               </form>
+
+              <div className="btn_criarConta">
+               <a href='#'>Criar conta</a>
+              </div>
             </div>
           }
 
