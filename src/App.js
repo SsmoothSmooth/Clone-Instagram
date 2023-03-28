b39aec4ad110733104fe170e79cc3d72ebc7aac2
José Wilson de Lima <josewilson791@gmail.com>
2023-03-27 22:27:31 -0300

    :sparkles: feat:
    Pagina de login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import {db} from './firebase.js';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  // Verificar em qual status o usuário se encontra logado ou não
+  const [user, setUser] = useState(null);
 
   useEffect(()=>{
   
@@ -11,7 +14,33 @@ function App() {
 
   return (
     <div className="App">
-      <h3>Teste aqui</h3>
+      
+      <div className="header">
+        <div className="center">
+
+          <div className="header_logo">
+            <a href='#'><img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"/></a>
+          </div> {/* header_logo */}
+
+          {
+            (user)?
+            <div>Hi</div>
+            :
+
+            <div className="header_loginForm">
+              <form>
+                <input type="text" placeholder="Login.."/>
+                <input type="password" placeholder="Senha.."/>
+                <input type="submit" name="acao" value="Logar!"/>
+              </form>
+            </div>
+          }
+
+          
+
+        </div> {/* center */}
+      </div> {/* header */}
+
     </div>
   );
 }
