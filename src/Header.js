83caef9215373b62dab385bf79c609cccc3f36e2
José Wilson de Lima <josewilson791@gmail.com>
2023-03-31 15:56:15 -0300

    :sparkles: feat: Sistema de post com upload de arquivo
    Criado modal para o sistema de post e sua estilizaçao. Desenvolvido duas funcionalidades, a primeira a function fecharModalUpload(); que serve para retirar o elemento com o className='modalUpload' do layout da página.A segunda function uploadPost(); quando enviado o form ele armazena o arquivo no Storage e o titulo, username, horarios e a url da imagem guarda no firestore

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
-import {auth} from './firebase.js'
+import firebase from 'firebase';
+import {auth, storage, db} from './firebase.js'
 
 function Header(props){
+
+    const [progress, setProgress] = useState(0);
+
+    const [file, setFile] = useState(null);
 
     useEffect(() => {
         
@@ -51,6 +56,13 @@ function Header(props){
         modal.style.display = "block";
     }
 
+    function abrirModalUpload(e){
+        e.preventDefault();
+
+        let modal = document.querySelector('.modalUpload');
+        modal.style.display = "block";
+    }
+
     function fecharModalCriar(){
 
         let modal = document.querySelector('.modalCriarConta');
@@ -59,12 +71,54 @@ function Header(props){
 
     }
 
+    function fecharModalUpload(){
+        let modal = document.querySelector('.modalUpload');
+        modal.style.display = "none";
+
+    }
+
+    function uploadPost(e){
+        e.preventDefault();
+
+        let tituloPost = document.getElementById('titulo-upload').value;
+
+        const uploadTask = storage.ref(`images/${file.name}`).put(file);
+
+        uploadTask.on("state_changed", function(snapshot){
+            const progress = Math.round(snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+            setProgress(progress);
+        }, function(error){
+
+        }, function(){
+            storage.ref("images").child(file.name).getDownloadURL()
+            .then(function(url){
+                db.collection('posts').add({
+                    titulo: tituloPost,
+                    image: url,
+                    userName: props.user,
+                    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+                })
+
+                setProgress(0);
+                setFile(null);
+
+                alert("Upload Finalizado !");
+
+                document.getElementById("form-upload").reset();
+
+            })
+        })
+
+
+
+    }
+
     return(
         <div className="header">
 
             <div className="modalCriarConta">
                 <div className="formCriarConta">
-                    <div onClick={()=>fecharModalCriar()} className="clsoe-modal-criar">X</div>
+                    <div onClick={()=>fecharModalCriar()} className="close-modal-criar">X</div>
                     <h2>Criar conta</h2>
                     <form onSubmit={(e)=> criarConta(e)}> 
                         <input id="email-cadastro" type="text" placeholder="Seu email" />
@@ -73,6 +127,20 @@ function Header(props){
                         <input type="submit" value="Criar conta !" />
                         
                     </form>
+                </div>
+            </div>
+
+            <div className="modalUpload">
+                <div className="formUpload">
+                    <div onClick={()=>fecharModalUpload()} className="close-modal-upload">X</div>
+                    <h2> Fazer Upload</h2>
+                    <form id="form-upload" onSubmit={(e)=>uploadPost(e)}>
+                        <progress id="progress-upload" value={progress}></progress>
+                        <input id="titulo-upload" type="text" placeholder="Nome da sua foto" />
+                        <input onChange={(e)=>setFile(e.target.files[0])} type="file" name="file" />
+                        <input type="submit" value="Postar" />
+                    </form>
+
                 </div>
             </div>
 
@@ -85,7 +153,7 @@ function Header(props){
                     (props.user)?
                     <div className="header_logadoInfo">
                     <span>Hi, <b>{props.user}</b></span>
-                    <a href="#">Postar</a>
+                    <a onClick={(e)=>abrirModalUpload(e)} href="#">Postar</a>
                     </div>
                     :
 
